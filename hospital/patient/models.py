ae5b6962c864b9958f3777026699769d88f7acae
sukhioo7 <deepsukhi943@gmail.com>
2024-02-21 15:14:17 +0530

    Connect to database

--- hospital/patient/models.py
+++ hospital/patient/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Patients(models.Model):
+    patient_id = models.AutoField(primary_key=True)
+    patient_name = models.CharField(max_length=300)
+    patient_age = models.SmallIntegerField()
+    patient_gender = models.CharField(max_length=30)
+    patient_city = models.CharField(max_length=300)
+    patient_email = models.EmailField(max_length=300)
+    patient_phone = models.CharField(max_length=10)
+    patient_symptoms = models.CharField(max_length=3000)
+
+
+
--- hospital/patient/views.py
+++ hospital/patient/views.py
@@ -1,22 +1,35 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 
 def home(request):
-    full_name = request.POST['full_name'] 
-    age = request.POST['age'] 
-    email = request.POST['email'] 
-    phone = request.POST['phone'] 
-    city = request.POST['city'] 
-    gender = request.POST['gender'] 
-    symptoms = request.POST['symptoms'] 
+    if request.POST:
+        full_name = request.POST['full_name'] 
+        age = request.POST['age'] 
+        email = request.POST['email'] 
+        phone = request.POST['phone'] 
+        city = request.POST['city'] 
+        gender = request.POST['gender'] 
+        symptoms = request.POST['symptoms'] 
 
-    data = {
-        'name':full_name,
-        'age':age,
-        'symptoms':symptoms
-    }
-    return render(request,'patient/home.html',context=data)
+        patient = models.Patients()
+        patient.patient_name = full_name
+        patient.patient_age = age
+        patient.patient_city = city
+        patient.patient_email = email
+        patient.patient_phone = phone
+        patient.patient_gender = gender
+        patient.patient_symptoms = symptoms
+        patient.save()
+
+        data = {
+            'msg':"Patient Registration Successfull"
+        }
+        return render(request,'patient/home.html',context=data)
+    else:
+        return render(request,'patient/home.html')
+
 
 def contact(request):
     return render(request,'patient/contact.html')

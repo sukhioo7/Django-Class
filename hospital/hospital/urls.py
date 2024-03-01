15df8971878e5100e1a4a7d859bf2260e260a213
sukhioo7 <deepsukhi943@gmail.com>
2024-03-01 15:11:26 +0530

    validations

--- hospital/hospital/urls.py
+++ hospital/hospital/urls.py
@@ -29,3 +29,4 @@ urlpatterns = [
 # www.hospial.com/
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- hospital/patient/views.py
+++ hospital/patient/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,HttpResponse,redirect
 from . import models
+import os
 
 # Create your views here.
 
@@ -10,22 +11,39 @@ def home(request):
         email = request.POST['email'] 
         phone = request.POST['phone'] 
         city = request.POST['city'] 
-        gender = request.POST['gender'] 
-        photo = request.FILES['photo']
+        gender = request.POST.get('gender') 
+        photo = request.FILES.get('photo')
         symptoms = request.POST['symptoms'] 
+        if all([full_name,age,email,phone,city,gender,photo,symptoms]):
+            check_email = models.Patients.objects.filter(patient_email=email).exists()
+            check_phone = models.Patients.objects.filter(patient_phone=phone).exists()
+            if not(check_email):
+                if not(check_phone):
+                    # patient = models.Patients()
+                    # patient.patient_name = full_name
+                    # patient.patient_age = age
+                    # patient.patient_city = city
+                    # patient.patient_email = email
+                    # patient.patient_phone = phone
+                    # patient.patient_gender = gender
+                    # patient.patient_image = photo
+                    # patient.patient_symptoms = symptoms
+                    # patient.save()
 
-        patient = models.Patients()
-        patient.patient_name = full_name
-        patient.patient_age = age
-        patient.patient_city = city
-        patient.patient_email = email
-        patient.patient_phone = phone
-        patient.patient_gender = gender
-        patient.patient_image = photo
-        patient.patient_symptoms = symptoms
-        patient.save()
-
-        return render(request,'patient/success.html')
+                    return render(request,'patient/success.html')
+                else:
+                    error = {
+                        'phone_exist' : True
+                    }
+            else:
+                error = {
+                    'email_exist' : True
+                }
+        else:
+            error = {
+                'empty_values':True
+            }
+        return render(request,'index.html',context=error)
     else:
         # select * from patients;
         patients = models.Patients.objects.all()
@@ -41,5 +59,7 @@ def contact(request):
 def delete(request,id):
     # select * from patients where patient_id=id;
     patient = models.Patients.objects.filter(patient_id=id).get()
+    file_path = patient.patient_image.path
+    os.remove(file_path)
     patient.delete()
     return redirect('patient:patient_home')

2dfa8afd7f54fb4fd2cb2801b5e0a8c0b3262833
sukhioo7 <deepsukhi943@gmail.com>
2024-10-08 18:51:38 +0530

    data backend to frontend

--- Amazon/Amazon/views.py
+++ Amazon/Amazon/views.py
@@ -4,7 +4,16 @@ def home(request):
     return render(request,'home.html')
 
 def about(request):
-    return render(request,'about.html')
+    who = 'Dr. Smith'
+    age = 24
+    profession = 'Doctor'
+
+    data = {
+        'person_name':who,
+        'age':age,
+        'profession':profession
+    }
+    return render(request,'about.html',context=data)
 
 def contact(request):
     return render(request,'contact.html')

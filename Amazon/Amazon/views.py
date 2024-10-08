851f6189555a2191bd1c597c4a7de1a78df09efc
sukhioo7 <deepsukhi943@gmail.com>
2024-10-08 18:34:09 +0530

    First Commit

---
+++ Amazon/Amazon/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import HttpResponse,render
+
+def home(request):
+    return render(request,'home.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')

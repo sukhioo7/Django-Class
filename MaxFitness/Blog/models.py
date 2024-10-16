3635b4cbbf97a3a3b433a9ec99083a818dc3e0c0
sukhioo7 <deepsukhi943@gmail.com>
2024-10-16 18:47:59 +0530

    making blog table

--- MaxFitness/Blog/models.py
+++ MaxFitness/Blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+
+    CATEGORY_CHOICES = [
+        ('Weight Loss', 'Weight Loss'),
+        ('Weight Gain', 'Weight Gain'),
+        ('Food', 'Food'),
+        ('Health', 'Health'),
+        ('Yoga', 'Yoga'),
+        ('Muscle Gain', 'Muscle Gain'),
+    ]
+
+    blog_id = models.AutoField(primary_key=True, unique=True)
+    title = models.CharField(max_length=3000)
+    category = models.CharField(max_length=300,choices=CATEGORY_CHOICES)
+    introduction = models.TextField(max_length=6000)
+    sub_heading1 = models.CharField(max_length=1000)
+    content1 = models.TextField(max_length=10000)
+    sub_heading2 = models.CharField(max_length=1000)
+    content2 = models.TextField(max_length=10000)
+    sub_heading3 = models.CharField(max_length=1000)
+    content3 = models.TextField(max_length=10000)
+    sub_heading4 = models.CharField(max_length=1000)
+    content4 = models.TextField(max_length=10000)
+    post_date = models.DateTimeField(auto_now=True)
--- MaxFitness/Blog/views.py
+++ MaxFitness/Blog/views.py
@@ -6,4 +6,6 @@ def blog_home(request):
     return render(request, 'Blog/home.html')
 
 def add_blog(request):
+    if request.method == 'POST':
+        print(request.POST)
     return render(request, 'Blog/add_blog.html')

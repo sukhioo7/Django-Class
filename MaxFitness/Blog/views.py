2560450eb43760ffe6884ef74459840e20059f29
sukhioo7 <deepsukhi943@gmail.com>
2024-10-17 18:46:51 +0530

    uploading blog to database

--- MaxFitness/Blog/views.py
+++ MaxFitness/Blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,HttpResponse
+from . import models
 
 # Create your views here.
 
@@ -7,5 +8,48 @@ def blog_home(request):
 
 def add_blog(request):
     if request.method == 'POST':
-        print(request.POST)
+        title = request.POST.get('title')
+        category = request.POST.get('category')
+        introduction = request.POST.get('introduction')
+        sub_heading1 = request.POST.get('sub_heading1')
+        sub_heading2 = request.POST.get('sub_heading2')
+        sub_heading3 = request.POST.get('sub_heading3')
+        sub_heading4 = request.POST.get('sub_heading4')
+        content1 = request.POST.get('content1')
+        content2 = request.POST.get('content2')
+        content3 = request.POST.get('content3')
+        content4 = request.POST.get('content4')
+
+        if all([title,category,introduction,sub_heading1,sub_heading2,sub_heading3,sub_heading4,
+                content1,content2,content3,content4]):
+            new_blog = models.Blog()
+            new_blog.title = title
+            new_blog.category = category
+            new_blog.introduction = introduction
+            new_blog.sub_heading1 = sub_heading1
+            new_blog.sub_heading2 = sub_heading2
+            new_blog.sub_heading3 = sub_heading3
+            new_blog.sub_heading4 = sub_heading4
+            new_blog.content1 = content1
+            new_blog.content2 = content2
+            new_blog.content3 = content3
+            new_blog.content4 = content4
+            new_blog.save()
+
+            data = {
+                'success': 'Blog successfully created'
+            }
+            return render(request, 'Blog/add_blog.html',context=data)
+
+            # OR 
+
+            # models.Blog.objects.create(title=title,category=category,introduction=introduction,
+            #                            sub_heading1=sub_heading1,sub_heading2=sub_heading2,sub_heading3=sub_heading3,
+            #                            sub_heading4=sub_heading4,content1=content1,content2=content2,content3=content3,
+            #                            content4=content4)
+
+        else:
+            print('Error: All fields are required.')
+
+
     return render(request, 'Blog/add_blog.html')
